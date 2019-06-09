94a03ededd151c4300c46892d08fd412d3003f81
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-09 21:51:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,8 +21,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT series.title, series.subgenre_id
-  FROM series"
+  "SELECT series.title, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
