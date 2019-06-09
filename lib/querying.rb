367381d13bc36893f6dcfdaaeed9890beee74970
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-09 21:36:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,10 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY characters.pecies
+  WHERE COUNT(characters.species) = MAX(COUNT(characters.species))"
 end
 
 def select_name_and_series_subgenres_of_authors
