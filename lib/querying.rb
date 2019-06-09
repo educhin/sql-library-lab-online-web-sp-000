8646171903dcb1e3d24499c97784247c041874dd
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-09 22:10:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,6 @@ def select_series_title_with_most_human_characters
   ON series.id = books.series_id
   JOIN characters
   ON characters.series_id = series.id
-
   GROUP BY series.id
   ORDER BY COUNT(characters.species) DESC
   LIMIT 1"
